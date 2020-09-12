58e5e882f24185c261f8ccacf4cf083953de30f6
AnshumanPadhi-3103 <42178537+AnshumanPadhi-3103@users.noreply.github.com>
2020-09-12 18:44:07 +0530

    Add files via upload

--- Assignment 4-Q2.py
+++ Assignment 4-Q2.py
@@ -1,6 +1,3 @@
-
-
-
 from Library import PartialPivot, print_imported_matrix, Identity_Aug, print_arrays
 
 
@@ -44,42 +41,41 @@ def forward_backward(a, b, n):
     return b
 
 
-def check_inverse_exists_and_calculate(A):  # function to check if inverse exists, and if so, calculate it
+def extract_A_B(Piv_AugM, n):  # function to extract A from pivoted matrix
+    A = [[0.0 for j in range(n)] for i in range(n)]
+    for j in range(n):
+        for i in range(n):
+            A[i][j] = Piv_AugM[i][j]
+
+    return A
+
+
+def check_inverse_exists_and_calculate(A, Piv_AugM):  # function to check if inverse exists, and if so, calculate it
     n = len(A)
+    L, U, A = lu_decomposition(A, n)  # Performing LU Decomposition
+    # Calculating determinant of the decomposed matrix
     det = 1
     for i in range(n):
         det = det * A[i][i]  # multiplying the diagonal elements
+    # for checking whether matrix is invertible or not
     if det == 0:  # since for a inverse to exist, det!=0
         print('Inverse Does not exist!')
     else:
         print('Inverse exists!')
         print('The Determinant value:', det)
-        Inverse_calculate(A, n)  # fucntion to calculate matrix
-    return
-
-
-def Inverse_calculate(Piv_AugM, n):
-    # Creating Blank Matrices and vectors
-    A = [[0 for j in range(n)] for i in range(n)]
-    inverse_A = [[0 for j in range(n)] for i in range(n)]
-    B = [0 for i in range(n)]
-
-    # extracting A from the augmented matrix
-    for j in range(n):
+        inverse_A = [[0 for j in range(n)] for i in range(n)]
+        # extracting B coulumn-wise and using it in forward backward substitution
         for i in range(n):
-            A[i][j] = Piv_AugM[i][j]
+            B = [0.0 for i in range(n)]
+            for j in range(n):
+                B[j] = Piv_AugM[j][i + n]
 
-    L, U, C = lu_decomposition(A, 4)  # LU decomposition on A
-    for i in range(n):  # extracting the columns of pivoted identity matrix
-        for j in range(n):
-            B[j] = Piv_AugM[j][i + n]
-            forward_backward(C, B, n)  # performing forward-backward substitution
-        for j in range(n):
-            inverse_A[j][i] = B[j]  # storing the solution columnwise
-
-    print('The inverse of A:')
-    print_arrays(inverse_A)
-    return inverse_A
+            B = forward_backward(A, B, n)
+            for j in range(n):  # Storing B in as inverse matrix coulmnwise
+                inverse_A[j][i] = B[j]
+        print('The Inverse is:')
+        print_arrays(inverse_A)
+        return inverse_A
 
 
 # calling and printing the matrix
@@ -87,33 +83,33 @@ F = open('InvA.txt', 'r')
 D = print_imported_matrix(F)
 
 AugM = Identity_Aug(D)  # Augmenting it with Identity matrix of same order
+
 Pivoted_AugM = PartialPivot(AugM, 4, 8)  # partial pivoting the augmented matrix
+A = extract_A_B(Pivoted_AugM, 4)
+p = check_inverse_exists_and_calculate(A, Pivoted_AugM)  # function to check if inverse exists, and if so, calculate it
 
-p = check_inverse_exists_and_calculate(Pivoted_AugM)  # function to check if inverse exists, and if so, calculate it
-
-#C:\Users\Anshuman\PycharmProjects\pythonProject1\venv\Scripts\python.exe C:/Users/Anshuman/PycharmProjects/pythonProject1/main.py
-#The Matrix:
+# C:\Users\Anshuman\PycharmProjects\pythonProject1\venv\Scripts\python.exe C:/Users/Anshuman/PycharmProjects/pythonProject1/main.py
+# The Matrix:
 #     0     2     8     6
 #     0     0     1     2
 #     0     1     0     1
 #     3     7     1     0
-#The Augmented Matrix:
+# The Augmented Matrix:
 #       0       2       8       6       1       0       0       0
 #       0       0       1       2       0       1       0       0
 #       0       1       0       1       0       0       1       0
 #       3       7       1       0       0       0       0       1
-#Matrix after partial pivot:
+# Matrix after partial pivot:
 #       3       7       1       0       0       0       0       1
 #       0       2       8       6       1       0       0       0
 #       0       0       1       2       0       1       0       0
 #       0       1       0       1       0       0       1       0
-#Inverse exists!
-#The Determinant value: 6
-#The inverse of A:
-#   0.101   0.173  -1.882   -0.12
-#  -0.033   0.165   0.857   0.063
-#  -0.024  -0.002  -0.317   0.037
-#   0.033  -0.165   0.143  -0.063
+# Inverse exists!
+# The Determinant value: 36.0
+#   -0.25   1.667  -1.833   0.333
+#   0.083  -0.667   0.833     0.0
+#   0.167  -0.333  -0.333     0.0
+#  -0.083   0.667   0.167     0.0
 
-#Process finished with exit code 0
+# Process finished with exit code 0
 
